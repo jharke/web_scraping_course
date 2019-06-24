e2899813f6e6a8d0203b41cd1cbf2839958e05cc
Julian Harke <julian.harke@wzb.eu>
2019-06-24 13:52:30 +0200

    Added example how to save data.

--- codes/APIs/rest_api.py
+++ codes/APIs/rest_api.py
@@ -1,5 +1,6 @@
 
 import requests
+import pandas as pd
 
 url = ('http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/'
        'nama_10_gdp?geo=EU28&precision=1&na_item=B1GQ&unit=CP_MEUR&'
@@ -8,5 +9,22 @@ url = ('http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/'
 resp = requests.get(url)
 resp_json = resp.json()
 
-resp_json['value']
-resp_json['dimension']['time']['category']['index']
+# Get the years that where returned
+gdp_years_index = resp_json['dimension']['time']['category']['index']
+gdp_years_index
+
+# Get the values that where returned
+gdp_value = resp_json['value']
+gdp_value
+
+# Function to access certain years value
+data = {'Year': list(gdp_years_index.keys()), 'GDP': list(gdp_value.values())}
+
+# Create DataFrame
+df = pd.DataFrame(data)
+
+# Print the output.
+df
+
+# Save to csv
+df.to_csv('eurostat_gdp.csv')
