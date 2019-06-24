9c6a48f5aa821ad9c5af0767392c25d5f8ba4fc5
Julian Harke <julian.harke@wzb.eu>
2019-06-24 15:33:17 +0200

    Added textpattern exercise

---
+++ codes/textpattern/textpattern_exercise.py
@@ -0,0 +1,17 @@
+
+import re
+import requests
+import datetime
+
+# Get src of the first 10 pages
+# Start with 'https://www.ebay-kleinanzeigen.de/s-gewerbeimmobilien/berlin/seite:1/c277l3331'
+
+# Find all prices, but drop prices per sqm
+prices = re.findall(..., src)
+prices
+
+# Find all dates in the format Day.Month.Year
+
+# Convert in pythons datetime format
+
+# Advanced: Find all dates including todays and yesterday entries
